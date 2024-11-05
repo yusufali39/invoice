dbb0382cb68c6241a59aa91a3e3fd17406a3e6e5
Yusuf Ali <84458764+yusufali39@users.noreply.github.com>
2024-11-05 08:03:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -209,7 +209,7 @@ $(document).ready(function () {
       2
     )}</span></h4> 
 
-    <h3 style="text-align: left;" class="mb-0">TOTAL AMOUNT: <span style="float: right; font-weight: 700; font-size: 33px;"> ₹${totalAmt.toFixed(
+    <h3 style="text-align: left;" class="mb-0">TOTAL AMOUNT: <span style="float: right; font-weight: 700; font-size: 30px;"> ₹${totalAmt.toFixed(
       2
     )}</span></h3>
 
