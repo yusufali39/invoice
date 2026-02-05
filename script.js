cd4f5f060bb79538e93f956172694695f4a5abfd
Yusuf Ali <84458764+yusufali39@users.noreply.github.com>
2026-02-05 13:53:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -258,7 +258,7 @@ $(document).ready(function () {
     <div class="container mt-1">
       <h3 class="shop-title text-center mb-0" id="savePdfButton"> 
       <img src="1770261049335.png" 
-     alt="logo" style="width:150px; height:auto; display:block; margin:0 auto;" /> </h3>
+     alt="logo" style="width:200px; height:80px; display:block; margin:0 auto;" /> </h3>
     
       
 
@@ -439,6 +439,7 @@ setTimeout(() => {
 
   
 });
+
 
 
 
